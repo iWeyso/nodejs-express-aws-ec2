4da575fde5c9366c340819801a3ab3a4ae4b839b
P Val <paul.abcd@outlook.fr>
2022-10-19 14:52:52 +0200

    ajout du projet

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) =>{
+
+    res.send('<h1> Express demo app </h1> <h4> Success ! </h4> <p> Version 1.0</p>');
+})
+
+app.get('/products', (req, res) => {
+
+    res.send([
+        {
+            productId: '101',
+            price: 100
+        },
+        {
+            productId: '102',
+            price: 150
+        }
+
+    ]);
+
+}
+)
+
+app.listen(port, () => {
+    console.log('Demo app listening to port ' + port);
+})
